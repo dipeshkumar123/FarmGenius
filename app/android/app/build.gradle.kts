daf46f8fb579c650412aff122b3415ccf6c522ed
dipeshkumar123 <panjiyardipesh123@gmail.com>
2026-06-27 17:04:32 +0530

    fix(android): resolve build issues by bumping flutter to 3.24 and using kts native config

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -9,6 +9,10 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    androidResources {
+        noCompress.add("tflite")
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -19,7 +23,7 @@ android {
         applicationId = "com.example.farmgenius"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
